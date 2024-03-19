46401cb074fe34199331ff93b24aead963fb765d
Oleg Ionov <ionov.dpmn@gmail.com>
2024-03-19 09:42:33 +0300

    Initial commit

---
+++ yandex_metrika_logs_api/__init__.py
@@ -0,0 +1,13 @@
+from yandex_metrika_logs_api.client import YandexMetrika
+from yandex_metrika_logs_api.constants import (
+    LogRequestSource,
+    LogRequestStatus,
+    AttributionModel,
+)
+
+__all__ = [
+    'YandexMetrika',
+    'LogRequestSource',
+    'LogRequestStatus',
+    'AttributionModel',
+]
